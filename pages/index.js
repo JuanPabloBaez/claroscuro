1216332b8a52d3d63b6b5beccb1a10370b950038
JP Baez <baezjuanpablo@gmail.com>
2022-08-26 00:46:18 +0200

    home setup

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
 import React,{useEffect, useState} from 'react';
+import Image from 'next/image';
 import {useRecoilState,useRecoilValue} from 'recoil';
 import {themeState,langState} from '../components/layout.js';
+import ReactPlayer from 'react-player';
+
+import Play from '../public/play-blanc.png'
+
 
 
 
 export default function Home() {
   const [,setLight] = useRecoilState(themeState);
   const [isPlaying, setIsPlaying] = useState(false);
-  const language = useRecoilValue(langState);
-
-  
-
+  const lang = useRecoilValue(langState);
 
 
     useEffect(()=>{
@@ -22,8 +24,22 @@ export default function Home() {
 
   return (
     <>
-    {isPlaying===false ? <button onClick={()=>setIsPlaying(true)} >watch reel</button>
-    :<button onClick={()=>setIsPlaying(false)} >Player</button>}
+    {isPlaying===false ? <button className='play' onClick={()=>setIsPlaying(true)} ><span>
+      <Image src={Play} alt="Press button to watch video reel"/>
+      <p>{lang==="esp"? "VER REEL" : "WATCH REEL"}</p>
+      </span></button>
+    :<div className='player'>
+      <span>
+      <button className='close-player' onClick={()=>setIsPlaying(false)} >X</button>
+      <ReactPlayer 
+        className="reel"
+        url="https://vimeo.com/585754763"
+        controls={true}
+        responsive="true"
+        
+        />
+        </span> 
+    </div>}
 
 
     <video loop autoPlay muted> <source src='/reel.mp4'/></video>
