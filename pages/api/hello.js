de2e55f51d53cfbe0ef55375524026f57f1622f2
JP Baez <baezjuanpablo@gmail.com>
2022-09-08 18:22:28 +0200

    trabajo page setup

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -8,8 +8,9 @@ export default function handler(req, res) {
     info_esp: "Escrito y dirigido por Juan Pablo Baez. Producido por Claroscuro Producciones." ,
     info_eng:"Writed and directed by Juan Pablo Baez. Produced by Claroscuro Producciones." ,
     prod_year: 2012 ,
-    photo:"public/photos/jardines.jpeg",
-    photoBW:"public/photos/jardines-2.jpeg"
+    photo:"/photos/jardines.jpg",
+    photoBW:"/photos/jardines-2.jpg",
+    link:""
     },
     {title_esp:"19/03" ,
       title_eng:"19/03" ,
@@ -18,8 +19,9 @@ export default function handler(req, res) {
       info_esp:"Escrito por Martín Román, dirigido por Iñaki Antuñano y Martín Román. Co producido por KILL THE TV y Claroscuro Producciones." ,
       info_eng:"Writed by Martín Román. Directed by Iñaki Antuñano and Martín Román. Produced by KILL THE TV and Claroscuro Producciones." ,
       prod_year: 2012 ,
-      photo:"public/photos/1903.jpeg",
-      photoBW:"public/photos/1903-2.jpeg"
+      photo:"/photos/1903.jpg",
+      photoBW:"/photos/1903-2.jpg",
+      link:"https://beatkino.com/#/film/19-03"
       },
       {title_esp:"LA PRIMERA GOLONDRINA" ,
         title_eng:"THE FIRST SWALLOW" ,
@@ -28,8 +30,9 @@ export default function handler(req, res) {
         info_esp:"escrito y dirigido por Lorena Lluch. Basado en un cuento de Albert Sánchez Piñol. Producido por Claroscuro Producciones." ,
         info_eng:"Writed and directed by Lorena Lluch. Based on the short story 'Només digues si encara m'estimes' by Albert Sánchez Piñol. Produced by Claroscuro Producciones." ,
         prod_year:2013 ,
-        photo:"public/photos/lpg.jpeg",
-        photoBW:"public/photos/lpg-2.jpeg"
+        photo:"/photos/lpg.jpg",
+        photoBW:"/photos/lpg-2.jpg",
+        link:"https://vimeo.com/117606822"
         },
         {title_esp:"RECREO" ,
           title_eng:"PLAYTIME" ,
@@ -38,8 +41,9 @@ export default function handler(req, res) {
           info_esp:"escrito y dirigido por Juan Pablo Báez. Producido por Claroscuro Producciones" ,
           info_eng:"Writed and directed by Juan Pablo Baez. Produced by Claroscuro Producciones." ,
           prod_year:2011 ,
-          photo:"public/photos/recreo.jpeg",
-          photoBW:"public/photos/recreo-2.jpeg"
+          photo:"/photos/recreo.jpg",
+          photoBW:"/photos/recreo-2.jpg",
+          link:"https://beatkino.com/#/film/recreo"
           },
           {title_esp:"THE HUNTING BIRD" ,
             title_eng:"THE HUNTING BIRD" ,
@@ -48,8 +52,9 @@ export default function handler(req, res) {
             info_esp:"dirigido por Juan Pablo Báez, Pau Martínez y Gabi Ochoa. Coproducido por Claroscuro Producciones y 2manyproducers." ,
             info_eng:"Directed by Juan Pablo Báez, Pau Martínez and Gabi Ochoa. Produced by Claroscuro Producciones and 2manyproducers." ,
             prod_year: 2010 ,
-            photo:"public/photos/polar.jpeg",
-        photoBW:"public/photos/polar-2.jpeg"
+            photo:"/photos/polar.jpg",
+            photoBW:"/photos/polar-2.jpg",
+            link:"https://beatkino.com/#/film/the_hunting_bird"
             }
     ])
 }
--- pages/trabajos.js
+++ pages/trabajos.js
@@ -1,15 +1,17 @@
 import React, {useEffect} from 'react';
+import Image from 'next/image';
 import useSWR from 'swr';
-import {useRecoilState} from 'recoil';
-import {themeState} from './index.js';
+import {useRecoilState,useRecoilValue} from 'recoil';
+import {themeState,langState} from './index.js';
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
 function Trabajos() {
     const [,setLight] = useRecoilState(themeState);
-    const { data, error } = useSWR('/api/hello', fetcher);
+    const lang = useRecoilValue(langState);
+    const {data, error } = useSWR('/api/hello', fetcher);
 
-    console.log(data)
+   
     useEffect(()=>{
         setLight(false)
     }, [setLight]);    
@@ -17,8 +19,27 @@ function Trabajos() {
     if (error) return <div>Failed to load</div>
     if (!data) return <div>Loading...</div>
 
+
   return (
-    <div>Trabajos</div>
+    <div>
+    <h4>{lang==="esp" ? "Selección de trabajos" : "Slected works"}</h4>
+    
+    {data.map((item, index)=>{
+       console.log(item)
+     return( <div className='item-container' key={index}>
+        <h2>{lang==="esp" ? item.title_esp : item.title_eng}</h2>
+        <p>{lang==="esp" ? item.cat_esp : item.cat_eng}</p>
+
+        <Image
+          src={item.photo}
+          alt={`frame of ${item.title_eng}`}
+          width={500}
+          height={500}
+        />
+      </div>)
+    })}
+    </div>
+    
   )
 }
 
